a20e9c0f9c6dae826a46fa3bdea26340cc2d0fef
Shiliang Zhang <lsftw13@gmail.com>
2020-11-14 19:30:42 -0800

    Improve variable naming in Utility.randomColor function

--- js/utility/utility.js
+++ js/utility/utility.js
@@ -17,15 +17,15 @@ var Utility = {
     },
     // Usage: randomColor('#59FF7D', '#486900')
     randomColor: function(color1, color2) {
-        var red1Hex = Utility.hexToDecimal(color1.slice(1, 3));
-        var green1Hex = Utility.hexToDecimal(color1.slice(3, 5));
-        var blue1Hex = Utility.hexToDecimal(color1.slice(5, 7));
-        var red2Hex = Utility.hexToDecimal(color2.slice(1, 3));
-        var green2Hex = Utility.hexToDecimal(color2.slice(3, 5));
-        var blue2Hex = Utility.hexToDecimal(color2.slice(5, 7));
-        var red = Utility.decimalToHex(Utility.randomInt(red1Hex, red2Hex));
-        var green = Utility.decimalToHex(Utility.randomInt(green1Hex, green2Hex));
-        var blue = Utility.decimalToHex(Utility.randomInt(blue1Hex, blue2Hex));
+        var redHex1 = Utility.hexToDecimal(color1.slice(1, 3));
+        var greenHex1 = Utility.hexToDecimal(color1.slice(3, 5));
+        var blueHex1 = Utility.hexToDecimal(color1.slice(5, 7));
+        var redHex2 = Utility.hexToDecimal(color2.slice(1, 3));
+        var greenHex2 = Utility.hexToDecimal(color2.slice(3, 5));
+        var blueHex2 = Utility.hexToDecimal(color2.slice(5, 7));
+        var red = Utility.decimalToHex(Utility.randomInt(redHex1, redHex2));
+        var green = Utility.decimalToHex(Utility.randomInt(greenHex1, greenHex2));
+        var blue = Utility.decimalToHex(Utility.randomInt(blueHex1, blueHex2));
         return '#' + red + green + blue;
     }
 };
